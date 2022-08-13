87644bc92f3d1b1203f4be0056353530bf88b086
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-08-13 22:38:10 +0000

    add: Create an else if statement for income exceeding 18800kr and less than 443200kr

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ function taxCalculate() {
 
     if (grossIncome.value < 0 && grossIncome.value < 18800) { // Bracket #1 - No income tax owed.
         tax = 0 * grossIncome.value;
+    } else if (grossIncome.value >= 18800 && grossIncome.value < 443200) { // Bracket #2 - 31% income taxes owed.
+        tax = 0.31 * grossIncome.value;
     }
 
 }
