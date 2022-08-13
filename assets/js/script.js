47679971a5c01ae3d004cd11ec280743aa776cbe
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-08-13 22:37:16 +0000

    add: Create a function for taxes and add an if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 let grossIncome = document.getElementById("income");
 let incomeTax = document.getElementById('tax');
+
+function taxCalculate() {
+
+    if (grossIncome.value < 0 && grossIncome.value < 18800) { // Bracket #1 - No income tax owed.
+        tax = 0 * grossIncome.value;
+    }
+
+}
